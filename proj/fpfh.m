4e498f598b21a67299dde6e905d896cbe9c53611
Cameron Long <cdalong@uvic.ca>
2018-03-13 22:51:19 -0700

    working feature calculations

---
+++ proj/fpfh.m
@@ -0,0 +1,9 @@
+function [outputArg1,outputArg2] = fpfh(features,inputArg2)
+%FPFH Summary of this function goes here
+%   Detailed explanation goes here
+
+% FPFH = SPF(p) + 1/k
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
