5b5781c7c41117800566c9f0e1b23945bd19f504
Cameron Long <cdalong@uvic.ca>
2018-03-13 22:23:54 -0700

    working feature calculations

--- proj/angular_variations.m
+++ proj/angular_variations.m
@@ -2,6 +2,10 @@ function [alpha,phi, theta] = angular_variations(u, v, w, nj, pi, pj)
 %ANGULAR_VARIATIONS Summary of this function goes here
 %   Detailed explanation goes here
 %these are the point pair features in 3-d space
+
+
+
+
 alpha = dot(v, nj);
 phi = (dot(u, (pj - pi)))/norm(pj - pi);
 theta = atan2(dot(w, nj), dot(u, nj));
--- proj/darboux_frame.m
+++ proj/darboux_frame.m
@@ -1,9 +1,23 @@
-function [u,v,w] = darboux_frame(pi ,pj, ni)
+function [u,v,w, pi, pj, ni, nj] = darboux_frame(pi ,pj, ni, nj)
 %DARBOUX_FRAME Summary of this function goes here
 %   Detailed explanation goes here
 %WE have to find the point with the lowest angle to the line between the
 %two points
 %this takes in a point and a k-neighbour, and calculates the uvn box
+
+
+theta1 = acosd(dot(ni, (pi - pj))); %angle of i's normal to line
+
+theta2 = acosd(dot(nj, (pj-pi))); %angle of j's normal
+
+if theta2 < theta1 %j is less, swap vectors
+    temp = pi;
+    tempn = ni;
+    pi = pj;
+    pj = temp;
+    ni = nj;
+    nj = tempn;
+end 
 
 u = ni;
 v = cross((pj - pi), u);
